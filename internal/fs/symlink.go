e184992efc6d559b1cf56768d6d5d4843c2f9257
Broken Pipe <navigaid@gmail.com>
2026-05-27 18:29:17 +0800

    use hackpadfs.ReadlinkFS

--- internal/fs/symlink.go
+++
@@ -1,9 +0,0 @@
-package fs
-
-import "github.com/hack-pad/hackpadfs"
-
-// ReadlinkFS is implemented by file systems that support reading symlink targets.
-type ReadlinkFS interface {
-hackpadfs.FS
-Readlink(name string) (string, error)
-}
