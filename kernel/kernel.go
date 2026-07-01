6cf3ad31059675964c930240faaf9a44d1209c57
Broken Pipe <navigaid@gmail.com>
2026-07-01 12:41:26 +0800

    add kernel

---
+++ kernel/kernel.go
@@ -0,0 +1,63 @@
+//go:build js
+
+package kernel
+
+import (
+	"path/filepath"
+	"syscall/js"
+
+	"github.com/hack-pad/hackpad/internal/fs"
+	"github.com/hack-pad/hackpad/internal/global"
+	"github.com/hack-pad/hackpad/internal/install"
+	"github.com/hack-pad/hackpad/internal/interop"
+	jsfs "github.com/hack-pad/hackpad/internal/js/fs"
+	"github.com/hack-pad/hackpad/internal/js/process"
+	"github.com/hack-pad/hackpad/internal/log"
+	libProcess "github.com/hack-pad/hackpad/internal/process"
+)
+
+func Init() {
+	process.Init()
+	jsfs.Init()
+	global.Set("dump", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		go func() {
+			basePath := ""
+			if len(args) >= 1 {
+				basePath = args[0].String()
+				if filepath.IsAbs(basePath) {
+					basePath = filepath.Clean(basePath)
+				} else {
+					basePath = filepath.Join(libProcess.Current().WorkingDirectory(), basePath)
+				}
+			}
+			var fsDump interface{}
+			if basePath != "" {
+				fsDump = jsfs.Dump(basePath)
+			}
+			log.Error("Process:\n", process.Dump(), "\n\nFiles:\n", fsDump)
+		}()
+		return nil
+	}))
+	global.Set("profile", js.FuncOf(interop.ProfileJS))
+	global.Set("install", js.FuncOf(install.InstallFunc))
+	global.Set("setWinch", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) < 3 {
+			return nil
+		}
+		termID := args[0].String()
+		cols := args[1].Int()
+		rows := args[2].Int()
+		xpx := 0
+		ypx := 0
+		if len(args) >= 4 {
+			xpx = args[3].Int()
+		}
+		if len(args) >= 5 {
+			ypx = args[4].Int()
+		}
+
+		fs.DispatchWinch(termID, cols, rows, xpx, ypx)
+		return nil
+	}))
+	interop.SetInitialized()
+}
